1b7a211f86b4f11ac39e8caedad51319f4025736
NunzioBasciano <nbasho1988@gmail.com>
2024-07-02 18:49:02 +0200

    fixed: font

--- 02-07-2024/Advice generator/src/components/card/Card.jsx
+++ 02-07-2024/Advice generator/src/components/card/Card.jsx
@@ -4,7 +4,7 @@ function Card(props) {
     return (
         <div className={styles.card}>
             <h1 className={styles.card_title}>ADVICE # {props.title}</h1>
-            <p className={styles.card_description}>{props.description}</p>
+            <p className={styles.card_description}>{'"' + props.description + '"'}</p>
             <img src="../public/images/divider.svg" alt="divider.icon" className={styles.card_icon_divider} />
             <button id='random' onClick={props.onClick} className={styles.card_icon_dice_button}>
                 <img src="../public/images/dice.svg" alt="dice.icon" className={styles.card_icon_dice} />
