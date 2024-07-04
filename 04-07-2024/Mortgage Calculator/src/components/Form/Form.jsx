1dae201a7b632f7e38b5b9b9bc30daa7a36a847a
NunzioBasciano <nbasho1988@gmail.com>
2024-07-04 21:55:29 +0200

    fix: show results

--- 04-07-2024/Mortgage Calculator/src/components/Form/Form.jsx
+++ 04-07-2024/Mortgage Calculator/src/components/Form/Form.jsx
@@ -1,5 +1,5 @@
 import styles from './form.module.css';
-import { useState } from 'react'
+import { useState } from 'react';
 
 function Form() {
 
@@ -10,43 +10,47 @@ function Form() {
         mortgage_type: ''
     });
 
+    const [result, setResult] = useState(null);
+
     function getInterest(obj) {
         const interest = ((+obj.mortgage_amount * +obj.interest_rate) / 100);
         return interest;
     }
 
     function getRepayment(obj) {
-        const interest = (getInterest(obj));
-        const repayment = ((+obj.mortgage_amount + +interest) / +obj.mortgage_term);
+        const interest = getInterest(obj);
+        const repayment = ((+obj.mortgage_amount + +interest) / +obj.mortgage_term / 12);
         return repayment;
     }
 
-
     function handleChange(e) {
         setInput({ ...input, [e.target.name]: e.target.value });
-        return input;
     }
 
-    function HandleSubmit(e) {
-        if (input.mortgage_amount !== '' && input.mortgage_term !== '' && input.interest_rate !== '' && input.mortgage_type === 'repayment') {
-            return getRepayment(input);
-        } else if (input.mortgage_amount !== '' && input.mortgage_term !== '' && input.interest_rate !== '' && input.mortgage_type === 'interest-only') {
-            return getInterest(input);
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (input.mortgage_amount !== '' && input.mortgage_term !== '' && input.interest_rate !== '' && input.mortgage_type !== '') {
+            let resultValue;
+            if (input.mortgage_type === 'repayment') {
+                resultValue = getRepayment(input);
+            } else if (input.mortgage_type === 'interest-only') {
+
+                resultValue = getInterest(input);
+            }
+            setResult(resultValue);
         } else {
-            alert('Please fill all the fields')
+            alert('Please fill in all fields');
         }
     }
 
-
     return (
-
         <>
             <div className={styles.form_section}>
                 <div className={styles.form_title_container}>
                     <h2>Mortgage Calculator</h2>
                     <a href="#" className="clear-link">Clear All</a>
                 </div>
-                <form className={styles.form}>
+                <form className={styles.form} onSubmit={handleSubmit}>
                     <div className={styles.form_Mortgage_amount}>
                         <label htmlFor="mortgage-amount">Mortgage Amount</label>
                         <input onChange={handleChange} className={styles.form_input} type="number" id="mortgage-amount" placeholder="£" name="mortgage_amount" />
@@ -64,28 +68,28 @@ function Form() {
                     <div className={styles.form_mortgage_type}>
                         <label>Mortgage Type</label>
                         <label className={styles.form_radio_input}>
-                            <input onChange={handleChange} type="radio" id="repayment" name="mortgage_type" value="repayment" />repayment
+                            <input onChange={handleChange} type="radio" id="repayment" name="mortgage_type" value="repayment" /> Repayment
                         </label>
                         <label className={styles.form_radio_input}>
-                            <input onChange={handleChange} type="radio" id="interest-only" name="mortgage_type" value="interest-only" />Interest Only
+                            <input onChange={handleChange} type="radio" id="interest-only" name="mortgage_type" value="interest-only" /> Interest Only
                         </label>
                     </div>
-                    <button onClick={HandleSubmit} className={styles.form_button} type="button" >Calculate Repayments</button>
-                </form >
-            </div >
+                    <button className={styles.form_button} type="submit">Calculate Repayments</button>
+                </form>
+            </div>
             <div className={styles.result_section}>
                 <h2 className={styles.result_title}>Your results</h2>
-                <p className={styles.result_paragraph}>Your result are shown below based on the information you provided. To adjust the result, edit the form and click "Calculate repayments" again.</p>
+                <p className={styles.result_paragraph}>Your result is shown below based on the information you provided. To adjust the result, edit the form and click "Calculate repayments" again.</p>
                 <div className={styles.result_container}>
-                    <h3 className={styles.result_secondary_heading}>Your monthly repayments</h3>
-                    <h1 className={styles.result_repayment}></h1>
+                    <h3 className={`${styles.result_secondary_heading} ${result === null ? styles.display_none : styles.display_block}`}>Your monthly repayments</h3>
+                    <h1 className={styles.result_repayment}>{result !== null ? `£${result.toFixed(2)}` : ''}</h1>
                     <hr />
                     <h3 className={styles.result_secondary_heading}>Total you'll repay over the term</h3>
+                    <h1 className={styles.result_repayment}>{result !== null ? `£${(result * input.mortgage_term).toFixed(2) * 12}` : ''}</h1>
                 </div>
-            </div>
+            </div >
         </>
-    )
+    );
 }
 
-
-export default Form
+export default Form;
