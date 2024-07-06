eeea928c2258ba2ef439ae5bcf30dd62f67da12e
NunzioBasciano <nbasho1988@gmail.com>
2024-07-06 19:17:14 +0200

    fix: css

--- 05-07-2024/Mortgage Calculator/src/App.jsx
+++ 05-07-2024/Mortgage Calculator/src/App.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
-import './App.css'
+import './styles/App.css'
 import Form from './components/Form/Form';
 import ResultComponent from './components/ResultComponent/ResultComponent';
 
-// Funzione principale del componente App
+
 function App() {
 
-  // Stato iniziale per i campi di input del form
   const inputDefault = {
     mortgage_amount: '',
     mortgage_term: '',
@@ -14,55 +13,40 @@ function App() {
     mortgage_type: ''
   }
 
-  // Stati per gestire gli input e i risultati
   const [input, setInput] = useState(inputDefault);
   const [totalRate, setTotalRates] = useState(null);
   const [totalMortgage, setTotalMortgage] = useState(null);
   const [totalMortgageInterest, setTotalMortgageInterest] = useState(null);
   const [isSubmitted, setIsSubmitted] = useState(false);
 
-  function   // Funzione per calcolare l'interesse
-    getInterest(interestRate, mortgage_amount) {
+  function getInterest(interestRate, mortgage_amount) {
     return parseFloat(mortgage_amount * interestRate / 100);
   }
 
-  // Funzione per calcolare il totale del mutuo
   function getTotal(mortgage_amount, interest) {
     return parseFloat(mortgage_amount) + parseFloat(interest);
   }
 
-  // Funzione per calcolare la rata mensile
   function getRates(total, mortgage_term) {
     return parseFloat(total / mortgage_term / 12);
   };
 
-  // Funzione gestita quando si invia il form
   function handleSubmit(e) {
     e.preventDefault();
-
-    // Controllo se tutti i campi del form sono completi
     if (input.mortgage_amount !== '' && input.mortgage_term !== '' && input.interest_rate !== '' && input.mortgage_type !== '') {
-
-      // Calcolo degli interessi, del totale e della rata
       const interest = getInterest(input.interest_rate, input.mortgage_amount);
       const total = getTotal(input.mortgage_amount, interest);
       const rate = getRates(total, input.mortgage_term);
-
-      // Imposta i risultati
       setTotalMortgage(total);
       setTotalMortgageInterest(interest);
 
-      // Imposta la rata solo se il tipo di mutuo è "repayment"
       if (input.mortgage_type === 'repayment') {
         setTotalRates(rate);
-        // altrimenti imposta null sulla rata 
       } else {
         setTotalRates(null);
       }
-      // Imposta lo stato isSubmitted a true
       setIsSubmitted(true);
     } else {
-      // Mostra un avviso se mancano campi da compilare
       alert('Please fill in all fields');
     }
   }
@@ -70,18 +54,8 @@ function App() {
   return (
     <>
       <div className='app'>
-        <Form
-          input={input}
-          setInput={setInput}
-          handleSubmit={handleSubmit}
-        />
-        <ResultComponent
-          input={input}
-          totalRate={totalRate}
-          totalMortgage={totalMortgage}
-          totalMortgageInterest={totalMortgageInterest}
-          isSubmitted={isSubmitted}
-        />
+        <Form input={input} setInput={setInput} handleSubmit={handleSubmit} />
+        <ResultComponent input={input} totalRate={totalRate} totalMortgage={totalMortgage} totalMortgageInterest={totalMortgageInterest} isSubmitted={isSubmitted} />
       </div>
     </>
   )
--- 05-07-2024/Mortgage Calculator/src/components/Form/Form.jsx
+++ 05-07-2024/Mortgage Calculator/src/components/Form/Form.jsx
@@ -1,24 +1,24 @@
 import styles from './form.module.css';
+/* import images from '../../../public/images/calculator.jpg' */
 
-// Componente Form per gestire l'inserimento dei dati del mutuo
+
 function Form({ input, setInput, handleSubmit }) {
 
-    // Viene chiamata ogni volta che l'utente modifica un input.
+    const inputDefault = {
+        mortgage_amount: '',
+        mortgage_term: '',
+        interest_rate: '',
+        mortgage_type: ''
+    };
+
     function handleChange(e) {
-
-        // Dichiara due variabili per gestire l'input name e value. ovvero const name = e.target.name e const value = e.target.value.
         const { name, value } = e.target;
-
-        // Viene modificata la variabile "input" con i nuovi valori inseriti dall'utente.
         setInput((prevState) => ({
-
-            // Ricopia dell'oggetto "input" con i nuovi valori inseriti dall'utente.
             ...prevState,
             [name]: value,
         }));
     }
 
-    //Resetta tutti i campi del form e i risultati.
     function clear() {
         setInput(inputDefault);
         setTotalRates(null);
@@ -32,17 +32,23 @@ function Form({ input, setInput, handleSubmit }) {
             <div className={styles.form_section}>
                 <div className={styles.form_title_container}>
                     <h2>Mortgage Calculator</h2>
-                    <a onClick={clear} href="#" className="clear-link">Clear All</a>
+                    <a onClick={clear} href="#">Clear All</a>
                 </div>
                 <form onSubmit={handleSubmit} className={styles.form}>
                     <div className={styles.form_Mortgage_amount}>
                         <label htmlFor="mortgage-amount">Mortgage Amount</label>
-                        <input onChange={handleChange} className={styles.form_input} type="number" placeholder="£" name="mortgage_amount" value={input.mortgage_amount} />
+                        <span className={styles.form_input_container}>
+                            <div className={styles.form_input_placeholder}>£</div>
+                            <input onChange={handleChange} className={styles.form_input} type="number" placeholder="" name="mortgage_amount" value={input.mortgage_amount} />
+                        </span>
                     </div>
                     <div className={styles.form_term_rate}>
                         <div className={styles.form_term_rate_item}>
                             <label htmlFor="mortgage-term">Mortgage Term</label>
-                            <input onChange={handleChange} className={styles.form_input} type="number" placeholder="years" name="mortgage_term" value={input.mortgage_term} />
+                            <span className={styles.form_input_container}>
+                                <input onChange={handleChange} className={styles.form_input} type="number" placeholder="" name="mortgage_term" value={input.mortgage_term} />
+                                <div className={styles.form_input_placeholder}>years</div>
+                            </span>
                         </div>
                         <div className={styles.form_term_rate_item}>
                             <label htmlFor="interest-rate">Interest Rate</label>
@@ -58,7 +64,10 @@ function Form({ input, setInput, handleSubmit }) {
                             <input onChange={handleChange} type="radio" name="mortgage_type" value="interest-only" /> Interest Only
                         </label>
                     </div>
-                    <input className={styles.form_button} type="submit" value='Calculate Repayments' />
+                    <span className={styles.form_button}>
+                        <img src='../../../public/images/calculator.svg' alt="calculator-image" />
+                        <input className={styles.form_input} type="submit" value='Calculate Repayments' />
+                    </span>
                 </form>
             </div>
         </>
